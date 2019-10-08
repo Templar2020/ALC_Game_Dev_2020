fa377efb08d5081185e0e8ebaaad73624f29c4e7
Mr. Powell <preston.powell@nebo.edu>
2019-10-08 15:21:29 -0600

    Added Prototype 2

---
+++ CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 15f;
+    public float xRange = 10.0f; 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > 10)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
+    }
+}

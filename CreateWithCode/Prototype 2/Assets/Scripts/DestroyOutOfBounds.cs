172b6bca0672aa3ecab1092bc2198d13d4caeea1
Mr. Powell <preston.powell@nebo.edu>
2019-10-14 08:15:29 -0600

    Update #2
    Player, Animals and food added
    Player, animals and food mechanics added

---
+++ CreateWithCode/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBounds = 30.0f;
+    public float lowerBounds = -10f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBounds)
+        {
+            Destroy(gameObject);
+
+        }
+        else if(transform.position.z < lowerBounds)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,19 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 15f;
-    public float xRange = 10.0f; 
+    public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
 
 
     // Update is called once per frame
     void Update()
     {
 
-        if (transform.position.x < -10)
+        if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
-        if (transform.position.x > 10)
+        if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
@@ -26,6 +28,12 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
 
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //When spacebar is pressed down run code
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
 
     }
 }
